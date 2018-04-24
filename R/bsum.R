521073d1ff79adc4c2f894c6b1feb19ea0453d37
nlxbach <nlxbach@gmail.com>
2018-04-24 12:16:07 -0300

    Add files via upload

---
+++ R/bsum.R
@@ -0,0 +1,19 @@
+#' @title Predicts Stock Price Movement for Given Stock Symbol
+#'
+#' @description this package for sum
+#'
+#' @param
+#'
+#' @return NULL
+#'
+#' @examples b.sum(3,4)
+#'
+#' @export b.sum
+
+
+b.sum <- function(a,b){
+  return(a+b)
+}
+hello <- function() {
+  print("Hello, world!")
+}
